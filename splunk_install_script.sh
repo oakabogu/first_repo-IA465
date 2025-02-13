9a53775d57f90c1dc4d38404158e30e857354614
oakabogu <oakabogu@emich.edu>
2025-02-12 20:30:43 -0500

    Add files via upload

---
+++ splunk_install_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+
+# Pull the latest version of Splunk Enterprise
+docker pull splunk/splunk:latest
+
+# Deploy a newly created Docker container for Splunk with privileged port 514 enabled
+# Default password is set to "password" with "admin" as the main user
+# This is for testing and development environments only
+# Accepts the "Free License for Splunk"
+docker run --name splunkfree \
+  --hostname splunkfree \
+  -p 8000:8000 \
+  -p 10000:10000/tcp \
+  -p 8080:8080 \
+  -p 514:514/tcp \
+  -p 9997:9997 \
+  --cap-add=NET_BIND_SERVICE \
+  -e "SPLUNK_PASSWORD=password" \
+  -e "SPLUNK_START_ARGS=--accept-license" \
+  -e "SPLUNK_LICENSE_URI=Free" \
+  -it splunk/splunk:latest
